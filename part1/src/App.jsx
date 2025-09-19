28e5771233fc3ab243a31156f40073f22e0be47b
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-09-19 09:30:14 +0300

    toka

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,28 @@
 import {useState} from 'react'
 
+const Button = ({onclick, text}) => <button onClick={onclick}>{text}</button>
+
+const Display = ({value}) => {
+    return (
+        <p>value: {value}</p>
+    )
+}
+
 const App = (props) => {
-    const [counter, setCounter] = useState(0);
+    const [value, setValue] = useState(0)
 
-    setTimeout(() => setCounter(counter + 1), 1000);
-
-    console.log(`rendering ${counter}`);
+    // Tämä on funktio tehdas. Sitä kutsutaan antamalla parametri jonka mukaan tehdas sitten luo ja
+    // ennen kaikkea PALAUTTAA FUNKTION
 
     return (
-        <div>{counter}</div>
+        <div>
+            <div>
+                <Display value={value} />
+                <Button onclick={ () => setValue(1000)} text={1000}/>
+                <Button onclick={ () => setValue(0)} text={"reset"}/>
+                <Button onclick={ () => setValue(value + 1)} text={"increment"}/>
+            </div>
+        </div>
     )
 }
 
