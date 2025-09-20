0bc343694651b510996f78312d4224e880672e2a
Jyrsinho <jyrgenhuhtala@gmail.com>
2025-09-20 17:02:44 +0300

    Puhelinluettelon nimisyötteen ja puhelinsyötteen validointi

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,33 +1,90 @@
 import { useState } from 'react'
 
+/**
+ * Validates the given form inputs and returns possible error. If there is no error,
+ * returns null.
+ * @param newName
+ * @param persons
+ * @param newNumber
+ */
+function validateForm(newName, persons, newNumber) {
+    let error = isValidName(newName, persons);
+    if (error !== null) return error;
+    error = isValidNumber(newNumber)
+    if (error !== null) return error;
+    return error;
+
+}
+
+function isValidName(newName, persons) {
+    const names = persons.map(person => person.name.toLowerCase());
+
+    if (newName === "write new name here") {
+        return("Add a new name, please");
+
+    }
+
+    if (newName.trim() === '') {
+        return ('New name cannot be empty string');
+    }
+
+    if (names.includes(newName.trim().toLowerCase())) {
+        return (`${newName} already exist`);
+    }
+
+    return null;
+}
+
+function isValidNumber(newNumber ) {
+    if (newNumber.trim().length < 1) {
+        return("Number cannot be empty")
+    }
+
+    const allowed = /^[0-9 -]/;
+    if (!allowed.test(newNumber)) {
+        return ("Invalid number");
+    }
+
+    return null;
+
+
+
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
-    const [newName, setNewName] = useState('write new name here')
+    const [newName, setNewName] = useState('write new name here');
+    const [newNumber, setNewNumber] = useState('');
+    const [error, setError] = useState(null);
 
-    const handleChange = (e) => {
-        setNewName(e.target.value);
+    const handleChangeName = (event) => {
+        setNewName(event.target.value);
+    }
+
+    const handleChangeNumber = (event) => {
+        setNewNumber(event.target.value);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const names = persons.map((person) => person.name.toLowerCase());
-        console.log(names);
+        const newError = validateForm(newName, persons, newNumber);
+        setError(newError);
+        console.log("error", error);
 
-        if (newName.trim() === '') {
-            alert('New name cannot be empty string');
-
-        } else if (names.includes(newName.toLowerCase())){
-            alert(`${newName} already exist`);
-
-        }else {
+        if (newError === null) {
             const newPerson = {
-                name: newName
+                name: newName.trim(),
+                number: newNumber,
             }
             setPersons(persons.concat([newPerson]))
             setNewName('');
+            setNewNumber('');
         }
     }
 
@@ -36,16 +93,20 @@ const App = () => {
             <h2>Phonebook</h2>
             <form>
                 <div>
-                    name: <input value={newName} onChange={handleChange} onFocus={() => setNewName('')} />
+                    name: <input value={newName} onChange={handleChangeName} onFocus={() => setNewName('')} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={handleChangeNumber} onFocus={() => setNewNumber('')} />
                 </div>
                 <div>
                     <button type="submit" onClick={handleSubmit}>add</button>
                 </div>
+                <p>{error}</p>
             </form>
             <h2>Numbers</h2>
             <ul>
                 {persons.map(person =>
-                <li key={person.name}>{person.name}</li>)}
+                <li key={person.name}>{person.name}: {person.number}</li>)}
             </ul>
 
         </div>
