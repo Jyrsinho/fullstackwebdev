7d5afed1866002a2496a41dc198680086a2be0c2
Jyrsinho <jyrgenhuhtala@gmail.com>
2025-09-26 13:52:49 +0300

    requests.http tiedosto ja sille environment

--- hello_node/index.js
+++ hello_node/index.js
@@ -20,12 +20,37 @@ let notes = [
     }
     ]
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
+    response.send('<h1>Hello Jyri!</h1>');
 })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes);
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const note = notes.find(note => note.id === id);
+    if (!note) {
+        response.json(note);
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body;
+    console.log(note);
+    response.json(note);
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    notes = notes.filter(note => note.id !== request.params.id);
+
+    response.status(204).end();
 })
 
 const Port = 3001;
