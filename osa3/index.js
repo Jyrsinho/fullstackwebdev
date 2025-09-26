0e99a309559f9ce28d4af1f4be9e137bd3ec7209
Jyrsinho <jyrgenhuhtala@gmail.com>
2025-09-26 16:34:25 +0300

    error handling in post method of phonebook

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-let phoneNumbers = [
+let persons = [
             {
                 "name": "Dan Abramov",
                 "number": "12-43-234345",
@@ -44,18 +44,18 @@ let phoneNumbers = [
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send(phoneNumbers);
+    res.send(persons);
 
 })
 
 app.get('/phoneNumbers', (req, res) => {
-    res.send(phoneNumbers);
+    res.send(persons);
 })
 
 
 app.get('/phoneNumbers/:id', (req, res) => {
     const id = req.params.id;
-    const phoneNumber = phoneNumbers.find(phoneNumber => phoneNumber.id === id);
+    const phoneNumber = persons.find(phoneNumber => phoneNumber.id === id);
 
     if (!phoneNumber) {
         res.status(404).send('Given ID Not Found');
@@ -68,19 +68,50 @@ app.get('/phoneNumbers/:id', (req, res) => {
 app.get('/info', (req, res) => {
     const info = `
     <div>
-        <p>Phonebook has info for ${phoneNumbers.length} people.</p>
+        <p>Phonebook has info for ${persons.length} people.</p>
         <p>${Date.now()}</p>
     `;
     res.send(info);
 })
 
+app.post("/phoneNumbers", (request, response) => {
+    console.log("persons prePost", persons);
+    const body = request.body;
+    console.log("request body", request.body)
+
+    if (!body.name || !body.number) {
+        return response.status(400).send('Content is missing');
+    }
+
+    if (persons.find(phoneNumber => phoneNumber.name === body.name)) {
+        console.log("Given name is already in use.");
+        return response.status(400).send({
+            error: 'Given name is already in use'
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID(),
+    }
+
+    persons = persons.concat([person]);
+    console.log("persons postPost", persons);
+    response.json(person);
+})
+
 app.delete('/phoneNumbers/:id', (req, res) => {
     const id = req.params.id;
 
-    phoneNumbers = phoneNumbers.filter(phoneNumber => phoneNumber.id !== id);
+    persons = persons.filter(phoneNumber => phoneNumber.id !== id);
     res.status(204).end();
 
 })
+
+const generateID = () => {
+    return Math.floor(Math.random() * 1000000);
+}
 
 const Port = 3001;
 app.listen(Port)
