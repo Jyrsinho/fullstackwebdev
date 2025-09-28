4489554f97136d96b6235e1f021975d97e5d8702
Jyrsinho <jyrgenhuhtala@gmail.com>
2025-09-28 13:57:38 +0300

    refactored filesystem

--- notesBackend/index.js
+++ notesBackend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 let notes = [
     {
@@ -20,6 +21,7 @@ let notes = [
     ]
 
 app.use(express.json());
+app.use(cors());
 
 
 app.get('/', (request, response) => {
