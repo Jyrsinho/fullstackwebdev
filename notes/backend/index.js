91bc31b7e6157a93e47c7cfba1c7242bbd49840f
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-10-06 12:57:00 +0300

    refactored directory structure

---
+++ notes/backend/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+const urlExample = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = ""
+
+let notes = [
+    {
+        id: "1",
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: "2",
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: "3",
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+    ]
+
+app.use(express.json());
+app.use(express.static('dist'));
+
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+})
+
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const note = notes.find(note => note.id === id);
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+app.post('/api/notes', (request, response) => {
+    console.log("notes before post", notes)
+    const body = request.body;
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'Content is missing'
+        });
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note);
+
+    response.json(note);
+    console.log("notes after post", notes);
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    notes = notes.filter(note => note.id !== id);
+
+    response.status(204).end();
+})
+
+
+const Port = process.env.PORT || 3001;
+app.listen(Port)
+console.log(`Server running on port ${Port}`)
+
+
+const generateId = () => {
+
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))): 0;
+    return String(maxId + 1);
+}
